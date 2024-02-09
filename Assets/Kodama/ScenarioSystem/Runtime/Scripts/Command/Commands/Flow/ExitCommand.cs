7599c2d85ffe363beb12ef41cfd27561bde55ef7
sankusa <91744435+sankusa@users.noreply.github.com>
2024-02-10 08:32:44 +0900

    Exitコマンド追加

---
+++ Assets/Kodama/ScenarioSystem/Runtime/Scripts/Command/Commands/Flow/ExitCommand.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Kodama.ScenarioSystem {
+    public class ExitCommand : CommandBase {
+        public override void Execute(ICommandService service) {
+            service.PagePlayProcess.JumpToEndIndex();
+        }
+    }
+}
